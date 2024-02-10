d4acdb9bc85441e7d0cc41e64c44d4c1a91e0c88
Mattias <mattias.kasto@gmail.com>
2024-02-10 03:37:21 +0100

    Handling dependencyInjection

--- DbAssignment/Program.cs
+++ DbAssignment/Program.cs
@@ -1,2 +1,11 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using DbAssignment.Contexts;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+var builder = Host.CreateDefaultBuilder().ConfigureServices(services =>
+{
+
+    services.AddDbContext<DataContext>(x => x.UseSqlServer(@"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=C:\VS-Projects\DbAssignment\DbAssignment\Data\DbForAssignment.mdf;Integrated Security=True;Connect Timeout=30;Encrypt=True"));
+
+}).Build();
