5275d8f47092982f13a3c4298064c7e25fb3b25b
Mattias <mattias.kasto@gmail.com>
2024-02-10 16:29:01 +0100

    Made Create for CRUD in Repo

---
+++ DbAssignment/Repositories/Repo.cs
@@ -0,0 +1,28 @@
+﻿using DbAssignment.Contexts;
+using System.Diagnostics;
+using System.Runtime.InteropServices;
+
+namespace DbAssignment.Repositories;
+
+internal class Repo<TEntity> where TEntity : class
+{
+    private readonly DataContext _context;
+
+    public Repo(DataContext context)
+    {
+        _context = context;
+    }
+
+    public virtual TEntity Create(TEntity entity)
+    {
+        try
+        {
+            _context.Set<TEntity>().Add(entity);
+            _context.SaveChanges();
+            return entity;
+        }
+        catch (Exception ex) { Debug.WriteLine(ex.Message); }
+        return null!;
+       
+    }
+}
